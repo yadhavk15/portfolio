44cf6b8867e3629dc05e863dfab14538e715bed7
yadhavk15 <yadhavk15@gmail.com>
2021-10-03 21:00:01 +0400

    Update

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,14 +2,14 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: 'Yadhav Kumar Nundlall | Computer Science Undergraduate', // e.g: 'Name | Developer'
+  title: 'Yadhav K. Nundlall Portfolio', // e.g: 'Name | Developer'
   lang: 'Creole, English, French, Hindi', // e.g: en, es, fr, jp
   description: 'My Online Portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: 'Computer Science Undergraduate',
+  title: 'An IT polymath who is always up to explore the sea of technologies it has to offer.',
   name: 'Yadhav Kumar Nundlall',
   subtitle: '',
   cta: '',
@@ -18,9 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
+  paragraphOne: 'Logical problem solving has always appealed to me and this explains my interests in mathematics, programming and computing in general. I also enjoy reading about scientific and tech on the Internet',
+  paragraphTwo: 'My geeky nature led me to pursue a Computer Science degree. Currently working as a DeVops and exploring the Open Source',
+  paragraphThree: 'Linux servers, Open Source Software and Fast Prototyping are what I like the most.',
   resume: '', // if no resume, the button will not show up
 };
 
@@ -28,53 +28,43 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'archi.jpg',
     title: '',
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'grafana.jpg',
     title: '',
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
+    img: 'git.jpg',
     title: '',
     info: '',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: '',
-  email: '',
+  btn: 'Email',
+  email: 'yadhavk15@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-//     {
-//       id: nanoid(),
-//       name: 'twitter',
-//       url: '',
-//     },
-//     {
-//       id: nanoid(),
-//       name: 'codepen',
-//       url: '',
-//     },
     {
       id: nanoid(),
       name: 'linkedin',
