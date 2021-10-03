e812117254c19f44e5c5c2d63518760805c5629e
yadhavk15 <yadhavk15@gmail.com>
2021-10-03 21:13:42 +0400

    text

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -49,15 +49,6 @@ const Projects = () => {
                         </p>
                         <p className="mb-4">{info2 || ''}</p>
                       </div>
-                      <a
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="cta-btn cta-btn--hero"
-                        href={url || '#!'}
-                      >
-                        See Live
-                      </a>
-
                       {repo && (
                         <a
                           target="_blank"
--- src/mock/data.js
+++ src/mock/data.js
@@ -9,7 +9,7 @@ export const headData = {
 
 // HERO DATA
 export const heroData = {
-  title: 'An IT polymath who is always up to explore the sea of technologies it has to offer.',
+  title: 'An computer polymath who is always up to explore the sea of technologies IT has to offer.',
   name: 'Yadhav Kumar Nundlall',
   subtitle: '',
   cta: '',
@@ -18,7 +18,7 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: 'Logical problem solving has always appealed to me and this explains my interests in mathematics, programming and computing in general. I also enjoy reading about scientific and tech on the Internet',
+  paragraphOne: 'Logical problem solving has always appealed to me and this explains my interests in mathematics, programming and computing in general. I also enjoy reading about science and tech on the Internet',
   paragraphTwo: 'My geeky nature led me to pursue a Computer Science degree. Currently working as a DeVops and exploring the Open Source',
   paragraphThree: 'Linux servers, Open Source Software and Fast Prototyping are what I like the most.',
   resume: '', // if no resume, the button will not show up
@@ -28,28 +28,28 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'archi.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'archi.jpeg',
+    title: 'Online Research Assitant Agent',
+    info: 'My Final Year Dissertation utilizing Google BERT Question Answering Algorithm, Web Scraping and NLP ',
+    info2: 'Its main aim is to facilitate research work.',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'grafana.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    title: 'Agentless Monitoring Tool',
+    info: 'A utility capable of monitoring the health of Linux servers with by simple means of SSH.',
+    info2: 'it sends the metrics to InfluxDB which is then graphed in realtime in Grafana',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'git.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    title: 'Fun Projects',
+    info: 'I have coded many tools during my course and intership which can be found in  my public Git repos.',
+    info2: 'Some cool ones include a Slack Curl uploader and a deadlock detector for Linux servers',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
@@ -58,8 +58,8 @@ export const projectsData = [
 // CONTACT DATA
 export const contactData = {
   cta: '',
-  btn: 'Email',
-  email: 'yadhavk15@gmail.com',
+  btn: 'yadhavk15@gmail.com',
+  email: '',
 };
 
 // FOOTER DATA
