cd271a23967e3bce7c0bc63901bbaf4173467e5d
yadhavk15 <47672413+yadhavk15@users.noreply.github.com>
2022-09-30 11:20:11 +0400

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -29,7 +29,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Your Name'}</span>
             <br />
-            {subtitle || "I'm a Junior DevOps Engineer at Redbox Digital."}
+            {subtitle || "I'm a DevOps Engineer at SQLI Digital Experience."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
